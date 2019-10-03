05e34a8a1ede2a7680ff2cb17154d21756c18348
Dalep <dalepaulsen.code.social@gmail.com>
2019-10-03 13:54:39 +1000

    added Haml, Devise, Simple_form gems and changed the index.html view with placeholder text

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem 'haml', '~> 5.1', '>= 5.1.2'
+gem 'simple_form', '~> 5.0'
+gem 'devise', '~> 4.7', '>= 4.7.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "welcome/index"
+  #root will set welcome view page and the index controller action as the homepage url (localhost:3000 atm)
+  #NOTE: "welcome is the folder name for the view file and index is the name of the view file which by convention has the same name as the index action in the controller!"
+  root "welcome#index"
 end
