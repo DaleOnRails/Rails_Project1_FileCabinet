5373af237e5594a25e5ce415d514c98d8230c335
Dalep <dalepaulsen.code.social@gmail.com>
2019-10-03 16:28:07 +1000

    created method to edit, destroy and reate docs with their html links

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -10,6 +10,7 @@ class DocsController < ApplicationController
   end
 
   #shows single doc, will create the show for view.
+  #show is already taken care of because of the before action.
   def show
   end
 
@@ -25,27 +26,40 @@ class DocsController < ApplicationController
     #creates a new document and also its parameters(its title and its content)
     @doc = Doc.new(doc_params)
 
-    #says if the doc is saved then get the application to redirect the user to the document that was just created/saved.
+    #says if the doc is saved then get the application to redirect the user to the document that was #just created/saved.
     if @doc.save
       redirect_to @doc
-      #says if it fails to save then render the doc again so the user can have another go at saving it.
-      #The reason why render 'new' is used here instead of 'redirect_to' is because redirect_to is a NEW http refresh. Meaning if the user has a long document and a save fails then if we used redireect_to they would lose all of their previous work!
-      #but render does not refresh the page. It keeps the user where they are and they get to have another go at saving the doc.
+      #    says if it fails to save then render the doc again so the user can have another go at saving it.
+      #    The reason why render 'new' is used here instead of 'redirect_to' is because redirect_to is a NEW http refresh. Meaning if the user has a long document and a save fails then if we used redireect_to they would lose all of their previous work!
+      #   but render does not refresh the page. It keeps the user where they are and they get to have another go at saving the doc.
     else
       render "new"
     end
   end
 
   #update action is responsible for editing views
+  #edit is already taken care of because of the before action.
   def edit
   end
 
+  # DIFFERENCE BETWEEN EDIT AND UPDATE:
+  # -EDIT is just responsible for the specific view file like show when doc paremeters are already created.
+
+  # -UPDATE however does the same  as the create method. It actually fetches that doc with the before action and then it changes the parameters
+
   #update does not have a view itself. it just makes changes to the databse using the edit view.
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render "edit"
+    end
   end
 
   #deletes the documents created
   def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
 
   #these private methods apply to this controllers code.
